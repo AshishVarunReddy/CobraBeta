f648f5a76d15524efe1414016d1aca61b7c66a61
iconofsin <martiantorso@gmail.com>
2025-07-08 06:43:27 +0530

    some changes

--- comp.c
+++ comp.c
@@ -6,46 +6,51 @@
 #include<string.h>
 
 typedef enum{
-    SEMI,
-    OPEN_PAREN,
-    CLOSED_PAREN
-} TypeSeperator;
-
-typedef enum{
-    EXIT,
-} TypeKeyword;
-
-typedef enum{
     INT,
-} TypeLiteral;
+    KEYWORD,
+    SEPARATOR,
+}Tokentype;
 
 
 typedef struct{
-    TypeKeyword type;
-}TokenKeyword;
+    Tokentype Type;
+    char* value;
+}Token;
 
-typedef struct{
-    TypeSeperator type;
-}TokenSeperator;
 
-typedef struct{
-    TypeLiteral type;
-    int value;
-}TokenLiteral;
+void print_token(Token* token){
+    printf("\nPrinting token...\n");
+    printf("value: %s\n", (token->value));
+    if(token->Type == INT){
+        printf("The type is INT\n");
+    }else if(token->Type == KEYWORD){
+        printf("The type is KEYWORD\n");
+    }else if(token->Type == SEPARATOR){
+        printf("The type is KEYWORD\n");
+    }else{
+        printf("Unknown type\n");
+    }
+    printf("\n");
+}
 
-TokenLiteral* gen_num(int* current_index, char* current){
-    TokenLiteral* literal = (TokenLiteral*)malloc(sizeof(TokenLiteral));
+Token* gen_num(int* current_index, char* current){
+    Token* literal = (Token*)malloc(sizeof(Token));
     int n = atoi(&current[*current_index]);
+    int count = 0;
     while(isdigit(current[*current_index]) && current[*current_index] != '\0'){
         (*current_index)++;
+        count++;
     }
-    literal->type = INT;
-    literal->value = n;
+    char* cf = (char*)malloc((count+1) * sizeof(char));
+    *(cf+count) = '\0';
+    sprintf(cf, "%d", n);
+    literal->Type = INT;
+    literal->value = cf;
     return literal;
 }
 
-TokenKeyword* gen_key(int* current_index, char* current){
-    TokenKeyword* key = (TokenKeyword*)malloc(sizeof(TokenKeyword));
+Token* gen_key(int* current_index, char* current){
+    Token* key = (Token*)malloc(sizeof(Token));
     char* keyword = (char*)malloc(sizeof(char)*32);
     int keyword_index = 0;
     while(isalpha(current[*current_index]) && current[*current_index] != '\0'){
@@ -60,14 +65,16 @@ TokenKeyword* gen_key(int* current_index, char* current){
     keyword_char[keyword_index] = '\0';
 
     if(!strcmp(keyword_char, "exit")){
-        printf("%s\n", keyword_char);
-        key->type = EXIT;
+        key->Type = KEYWORD;
+        key->value = keyword;
     }
-    free(keyword);
     return key;
 }
 
-void lexer(FILE* fp){
+int token_index = 0;
+
+
+Token** lexer(FILE* fp){
     char* buffer = 0;
     fseek(fp, 0, SEEK_END);
     long len = ftell(fp);
@@ -78,22 +85,44 @@ void lexer(FILE* fp){
     char* current = (char*)malloc(sizeof(char)*len + 1);
     printf("Buffer: %s", buffer);
     current = buffer;
-    TokenLiteral* test_tok;
-    TokenKeyword* key;
     int current_index = 0;
+    Token** tokenArray = (Token**)malloc(sizeof(Token*)*2);
     while(current[current_index]!='\0'){
+        if(token_index+1 > 2){
+            tokenArray = (Token**)realloc(tokenArray, (token_index+1)*sizeof(Token*));
+        }
         if(isdigit(current[current_index])){
-           test_tok = gen_num(&current_index, current);
+           Token* test_tok = gen_num(&current_index, current);
+           print_token(test_tok);
+           tokenArray[token_index++] = test_tok;
+           current_index--;
         }else if(isalpha(current[current_index])){
-            key = gen_key(&current_index, current);
+            Token* key = gen_key(&current_index, current);
+            print_token(key);
+            tokenArray[token_index++] = key;
+            current_index--;
+        }else if(current[current_index] == ';'){
+            Token* semi = (Token*)malloc(sizeof(Token));
+            semi->value = ";";
+            semi->Type = SEPARATOR;
+            print_token(semi);
+            tokenArray[token_index++] = semi;
+        }else if(current[current_index] == '('){
+            Token* openP = (Token*)malloc(sizeof(Token));
+            openP->value = "(";
+            openP->Type = SEPARATOR;
+            print_token(openP);
+            tokenArray[token_index++] = openP;
+        }else if(current[current_index] == ')'){
+            Token* closeP = (Token*)malloc(sizeof(Token));
+            closeP->value = ")";
+            closeP->Type = SEPARATOR;
+            print_token(closeP);
+            tokenArray[token_index++] = closeP;
         }
         current_index++;
     }
-    printf("The number given is %d\n", test_tok->value);
-    if(key->type == EXIT)
-        printf("process exiting...\n");
-    free(key);
-    free(test_tok);
+    return tokenArray;
 }
 
 
@@ -104,8 +133,10 @@ int main(){
       perror("File not opened!!\n");
       exit(-1);
   }
-
-  lexer(fp);
+  Token** tokenArray = (Token**)malloc(sizeof(Token*) * token_index);
+  tokenArray = lexer(fp);
+  print_token(tokenArray[1]);
   fclose(fp);
+  free(tokenArray);
   return 0;
 }
