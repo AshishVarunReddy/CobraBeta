0cb899d5e01cc415ca15fd10b2a262630dbc89ae
AshishVarunReddy <ashishvarun1609@gmail.com>
2025-10-10 22:25:38 +0530

    Minor changes

--- assembly/gencra.asm
+++ assembly/gencra.asm
@@ -7,7 +7,12 @@ mov rax, 3
 mov r10, rax
 sub rsp, 4
 mov dword [rbp-4], 3
-mov r10, [rbp - 4]
+mov rax, 4
+add rax, 0
+mov r10, rax
+sub rsp, 4
+mov dword [rbp-8], 4
+mov r10, [rbp - 8]
 exit:
 mov rax, 60
 mov rdi, r10
